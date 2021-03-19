aa4aaf3e62b75432c71f1847b242dccbe4dd76ef
Nathan Moritz <nateim3@gmail.com>
2021-03-19 09:38:32 -0400

    Added loop

--- src/Routes/Routes.go
+++ src/Routes/Routes.go
@@ -50,129 +50,127 @@ var realms = map[string]interface{}{
   "Blade's Edge": "blade's edge",
   "Bleeding Hollow": "bleeding hollow",
   "Blood Furnace": "blood furnace",
-  "bloodhoof">Bloodhoof</option>
-  "bloodscalp">Bloodscalp</option>
-  "bonechewer">Bonechewer</option>
-  "borean tundra">Borean Tundra</option>
-  "boulderfist">Boulderfist</option>
-  "bronzebeard">Bronzebeard</option>
-  "burning blade">Burning Blade</option>
-  "burning legion">Burning Legion</option>
-  "cairne">Cairne</option>
-  "cenarion circle">Cenarion Circle</option>
-  "cenarius">Cenarius</option>
-  "cho'gall">Cho'gall</option>
-  "chromaggus">Chromaggus</option>
-  "coilfang">Coilfang</option>
-  "crushridge">Crushridge</option>
-  "daggerspine">Daggerspine</option>
-  "dalaran">Dalaran</option>
-  "dalvengyr">Dalvengyr</option>
-  "dark iron">Dark Iron</option>
-  "darkspear">Darkspear</option>
-  "darrowmere">Darrowmere</option>
-  "dawnbringer">Dawnbringer</option>
-  "deathwing">Deathwing</option>
-  "demon soul">Demon Soul</option>
-  "dentarg">Dentarg</option>
-  "destromath">Destromath</option>
-  "dethecus">Dethecus</option>
-  "detheroc">Detheroc</option>
-  "doomhammer">Doomhammer</option>
-  "draenor">Draenor</option>
-  "dragonblight">Dragonblight</option>
-  "dragonmaw">Dragonmaw</option>
-  "drak'tharon">Drak'Tharon</option>
-  "drak'thul">Drak'thul</option>
-  "draka">Draka</option>
-  "drenden">Drenden</option>
-  "dunemaul">Dunemaul</option>
-  "durotan">Durotan</option>
-  "duskwood">Duskwood</option>
-  "earthen ring">Dethecus</option>
-  "earthen ring">Earthen Ring</option>
-  "echo isles">Echo Isles</option>
-  "eitrigg">Eitrigg</option>
-  "eldre'thalas">Eldre'Thalas</option>
-  "elune">Elune</option>
-  "emerald dream">Emerald Dream</option>
-  "eonar">Eonar</option>
-  "eredar">Eredar</option>
-  "executus">Executus</option>
-  "exodar">Exodar</option>
-  "farstriders">Farstriders</option>
-  "feathermoon">Feathermoon</option>
-  "fenris">Fenris</option>
-  "firetree">Firetree</option>
-  "fizzcrank">Fizzcrank</option>
-  "frostmane">Frostmane</option>
-  "frostwolf">Frostwolf</option>
-  "galakrond">Galakrond</option>
-  "garithos">Garithos</option>
-  "garona">Garona</option>
-  "garrosh">Garrosh</option>
-  "ghostlands">Ghostlands</option>
-  "gilneas">Gilneas</option>
-  "gnomeregan">Gnomeregan</option>
-  "frostwolf">Frostwolf</option>
-  "gorefiend">Gorefiend</option>
-  "gorgonnash">Gorgonnash</option>
-  "greymane">Greymane</option>
-  "grizzly hills">Grizzly Hills</option>
-  "gul'dan">Gul'dan</option>
-  "gurubashi">Gurubashi</option>
-  "hakkar">Hakkar</option>
-  "haomarush">Haomarush</option>
-  "hellscream">Hellscream</option>
-  "hydraxis">Hydraxis</option>
-  "hyjal">Hyjal</option>
-  "icecrown">Icecrown</option>
-  "illidan">Illidan</option>
-  "jaedenar">Jaedenar</option>
-  "kael'thas">Kael'thas</option>
-  "kalecgos">Kalecgos</option>
-  "kargath">Kargath</option>
-  "kel'thuzad">Kel'Thuzad</option>
-  "khadgar">Khadgar</option>
-  "khaz modan">Khaz Modan</option>
-  "kil'jaeden">Kil'jaeden</option>
-  "Kilrogg">kilrogg</option>
-  "korgath">Korgath</option>
-  "korialstrasz">Korialstrasz</option>
-  "kul tiras">Kul Tiras</option>
-  "laughing skull">Laughing Skull</option>
-  "lethon">Lethon</option>
-  "lightbringer">Lightbringer</option>
-  "lightning's blade">Lightning's Blade</option>
-  "lightninghoof">Lightninghoof</option>
-  "llane">Llane</option>
-  "lothar">Lothar</option>
-  "madoran">Madoran</option>
-  "maelstrom">Maelstrom</option>
-  "magtheridon">Magtheridon</option>
-  "maiev">Maiev</option>
-  "mal'ganis">Mal'Ganis</option>
-  "malfurion">Malfurion</option>
-  "malorne">Malorne</option>
-  "malygos">Malygos</option>
-  "mannoroth">Mannoroth</option>
-  "medivh">Medivh</option>
-  "misha">Misha</option>
-  "mok'nathal">Mok'Nathal</option>
-  "moon guard">Moon Guard</option>
-  "moonrunner">Moonrunner</option>
-  "mug'thol">Mug'thol</option>
-  "muradin">Muradin</option>
-  "nathrezim">Nathrezim</option>
-  "nazgrel">Nazgrel</option>
-  "nazjatar">Nazjatar</option>
-  "ner'zhul">Ner'zhul</option>
-  "nesingwary">Nesingwary</option>
-  "nordrassil">Nordrassil</option>
-  "norgannon">Norgannon</option>
-  "onyxia">Onyxia</option>
-  "perenolde">Perenolde</option>
-  "proudmoore">Proudmoore</option>
+  "Bloodhoof": "bloodhoof",
+  "Bloodscalp": "bloodscalp",
+  "Bonechewer": "bonechewer",
+  "Borean Tundra": "borean tundra",
+  "Boulderfist": "boulderfist",
+  "Bronzebeard": "bronzebeard",
+  "Burning Blade": "burning blade",
+  "Burning Legion": "burning legion",
+  "Cairne": "cairne",
+  "Cenarion Circle": "cenarion circle",
+  "Cenarius": "cenarius",
+  "Cho'gall": "cho'gall",
+  "Chromaggus": "chromaggus",
+  "Coilfang": "coilfang",
+  "Crushridge": "crushridge",
+  "Daggerspine": "daggerspine",
+  "Dalaran": "dalaran",
+  "Dalvengyr": "dalvengyr",
+  "Dark Iron": "dark iron",
+  "Darkspear": "darkspear",
+  "Darrowmere": "darrowmere",
+  "Dawnbringer": "dawnbringer",
+  "Deathwing": "deathwing",
+  "Demon Soul": "demon soul",
+  "Dentarg": "dentarg",
+  "Destromath": "destromath",
+  "Dethecus": "dethecus",
+  "Detheroc": "detheroc",
+  "Doomhammer": "doomhammer",
+  "Draenor": "draenor",
+  "Dragonblight": "dragonblight",
+  "Dragonmaw": "dragonmaw",
+  "Drak'Tharon": "drak'tharon",
+  "Drak'thul": "drak'thul",
+  "Draka": "draka",
+  "Drenden": "drenden",
+  "Dunemaul": "dunemaul",
+  "Durotan": "durotan",
+  "Duskwood": "duskwood",
+  "Earthen Ring": "earthen ring",
+  "Echo Isles": "echo isles",
+  "Eitrigg": "eitrigg",
+  "Eldre'Thalas": "eldre'thalas",
+  "Elune": "elune",
+  "Emerald Dream": "emerald dream",
+  "Eonar": "eonar",
+  "Eredar": "eredar",
+  "Executus": "executus",
+  "Exodar": "exodar",
+  "Farstriders": "farstriders",
+  "Feathermoon": "feathermoon",
+  "Fenris": "fenris",
+  "Firetree": "firetree",
+  "Fizzcrank": "fizzcrank",
+  "Frostmane": "frostmane",
+  "Frostwolf": "frostwolf",
+  "Galakron": "galakrond",
+  "Garithos": "garithos",
+  "Garona": "garona",
+  "Garrosh": "garrosh",
+  "Ghostlands": "ghostlands",
+  "Gilneas": "gilneas",
+  "Gnomeregan": "gnomeregan",
+  "Gorefiend": "gorefiend",
+  "Gorgonnash": "gorgonnash",
+  "Greymane": "greymane",
+  "Grizzly Hills": "grizzly hills",
+  "Gul'dan": "gul'dan",
+  "Gurubashi": "gurubashi",
+  "Hakkar": "hakkar",
+  "Haomarush": "haomarush",
+  "Hellscream": "hellscream",
+  "Hydraxis": "hydraxis",
+  "Hyjal": "hyjal",
+  "Icecrown": "icecrown",
+  "Illidan": "illidan",
+  "Jaedenar": "jaedenar",
+  "Kael'thas": "kael'thas",
+  "Kalecgos": "kalecgos",
+  "Kargath": "kargath",
+  "Kel'Thuzad": "kel'thuzad",
+  "Khadgar": "khadgar",
+  "Khaz Modan": "khaz modan",
+  "Kil'jaeden": "kil'jaeden",
+  "Kilrogg": "kilrogg",
+  "Korgath": "korgath",
+  "Korialstrasz": "korialstrasz",
+  "Kul Tiras": "kul tiras",
+  "Laughing Skull": "laughing skull",
+  "Lethon": "lethon",
+  "Lightbringer": "lightbringer",
+  "Lightning's Blade": "lightning's blade",
+  "Lightninghoof": "lightninghoof",
+  "Llane": "llane",
+  "Lothar": "lothar",
+  "Madoran": "madoran",
+  "Maelstrom": "maelstrom",
+  "Magtheridon": "magtheridon",
+  "Maiev": "maiev",
+  "Mal'Ganis": "mal'ganis",
+  "Malfurion": "malfurion",
+  "Malorne": "malorne",
+  "Malygos": "malygos",
+  "Mannoroth": "mannoroth",
+  "Medivh": "medivh",
+  "Misha": "misha",
+  "Mok'Nathal": "mok'nathal",
+  "Moon Guard": "moon guard",
+  "Moonrunner": "moonrunner",
+  "Mug'thol": "mug'thol",
+  "Muradin": "muradin",
+  "Nathrezim": "nathrezim",
+  "Nazgrel": "nazgrel",
+  "Nazjatar": "nazjatar",
+  "Ner'zhul": "ner'zhul",
+  "Nesingwary": "nesingwary",
+  "Nordrassil": "nordrassil",
+  "Norgannon": "norgannon",
+  "Onyxia": "onyxia",
+  "Perenolde": "perenolde",
+  "Proudmoore": "proudmoore",
   "Quel'Thalas": "quel'thalas",
   "Quel'dorei": "quel'dorei",
   "Ragnaros": "ragnaros",
@@ -207,13 +205,13 @@ var realms = map[string]interface{}{
   "Stonemaul": "stonemaul",
   "Stormrage": "stormrage",
   "Stormreaver": "stormreaver",
-  "Stormscale": "stormscale"
+  "Stormscale": "stormscale",
   "Suramar": "suramar",
   "Tanaris": "tanaris",
   "Terokkar": "terokkar",
   "Thaurissan": "thaurissan",
   "The Forgotten Coast": "the forgotten coast",
-  "The Scryers": "the scryers"
+  "The Scryers": "the scryers",
   "The Underbog": "the underbog",
   "The Venture Co": "the venture co",
   "Thorium Brotherhood": "thorium brotherhood",
@@ -242,9 +240,9 @@ var realms = map[string]interface{}{
   "Wyrmrest Accord": "wyrmrest accord",
   "Ysera": "ysera",
   "Ysondre": "ysondre",
-  "Zangarmarsh": "zangarmarsh"
+  "Zangarmarsh": "zangarmarsh",
   "Zul'jin": "zul'jin",
-  "Zuluhead": "zuluhead"
+  "Zuluhead": "zuluhead",
 }
 
 // SetupRouter ... Configure routes

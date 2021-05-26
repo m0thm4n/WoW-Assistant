a86b75ad6f076883dea42aa4da9a4523fdb63604
Nathan Moritz <nateim3@gmail.com>
2021-05-26 10:54:12 -0400

    Added mongo-db driver support

---
+++ python/db-builder/realm_name.py
@@ -0,0 +1,462 @@
+def get_realm_name(arg):
+    realm_name = ""
+
+    if arg == "aegwynn":
+        realm_name = "aegwynn"
+    elif arg == "aerie peak":
+        realm_name = "aerie_peak"
+    elif arg == "agamaggan":
+        realm_name = "agamaggan"
+    elif arg == "aggramar":
+        realm_name = "aggramar"
+    elif arg == "akama":
+        realm_name = "akama"
+    elif arg == "alexstrasza":
+        realm_name = "alexstrasza"
+    elif arg == "alleria":
+        realm_name = "alleria"
+    elif arg == "altar of storms":
+        realm_name = "altar_of_storms"
+    elif arg == "alterac mountains":
+        realm_name = "alterac_mountains"
+    elif arg == "andorhal":
+        realm_name = "andorhal"
+    elif arg == "anetheron":
+        realm_name = "anetheron"
+    elif arg == "antonidas":
+        realm_name = "antonidas"
+    elif arg == "anub'arak":
+        realm_name = "anub_arak"
+    elif arg == "anvilmar":
+        realm_name = "anvilmar"
+    elif arg == "arathor":
+        realm_name = "arathor"
+    elif arg == "archimonde":
+        realm_name = "archimonde"
+    elif arg == "area 52":
+        realm_name = "area_52"
+    elif arg == "argent dawn":
+        realm_name = "argent_dawn"
+    elif arg == "arthas":
+        realm_name = "arthas"
+    elif arg == "arygos":
+        realm_name = "arygos"
+    elif arg == "auchindoun":
+        realm_name = "auchindoun"
+    elif arg == "azgalor":
+        realm_name = "azgalor"
+    elif arg == "azjol-nerub":
+        realm_name = "azjol_nerub"
+    elif arg == "azshara":
+        realm_name = "azshara"
+    elif arg == "azuremyst":
+        realm_name = "azuremyst"
+    elif arg == "baelgun":
+        realm_name = "baelgun"
+    elif arg == "balnazzar":
+        realm_name = "balnazzar"
+    elif arg == "black dragonflight":
+        realm_name = "black_dragonflight"
+    elif arg == "blackhand":
+        realm_name = "blackhand"
+    elif arg == "blackrock":
+        realm_name = "blackrock"
+    elif arg == "blackwater-raiders":
+        realm_name = "blackwater_raiders"
+    elif arg == "blackwing lair":
+        realm_name = "blackwing_lair"
+    elif arg == "blades-edge":
+        realm_name = "blades_edge"
+    elif arg == "bleeding hollow":
+        realm_name = "bleeding_hollow"
+    elif arg == "blood furnace":
+        realm_name = "blood_furnace"
+    elif arg == "bloodhoof":
+        realm_name = "bloodhoof"
+    elif arg == "bloodscalp":
+        realm_name = "bloodscalp"
+    elif arg == "bonechewer":
+        realm_name = "bonechewer"
+    elif arg == "borean tundra":
+        realm_name = "borean_tundra"
+    elif arg == "boulderfist":
+        realm_name = "boulderfist"
+    elif arg == "bronzebeard":
+        realm_name = "bronzebeard"
+    elif arg == "burning blade":
+        realm_name = "burning_blade"
+    elif arg == "burning legion":
+        realm_name = "burning_legion"
+    elif arg == "cairne":
+        realm_name = "cairne"
+    elif arg == "cenarion circle":
+        realm_name = "cenarion_circle"
+    elif arg == "cenarius":
+        realm_name = "cenarius"
+    elif arg == "cho'gall":
+        realm_name = "cho_gall"
+    elif arg == "chromaggus":
+        realm_name = "chromaggus"
+    elif arg == "coilfang":
+        realm_name = "coilfang"
+    elif arg == "crushridge":
+        realm_name = "crushridge"
+    elif arg == "daggerspine":
+        realm_name = "daggerspine"
+    elif arg == "dalaran":
+        realm_name = "dalaran"
+    elif arg == "dalvengyr":
+        realm_name = "dalvengyr"
+    elif arg == "dark iron":
+        realm_name = "dark_iron"
+    elif arg == "darkspear":
+        realm_name = "darkspear",
+    elif arg == "darrowmere":
+        realm_name = "darrowmere"
+    elif arg == "dawnbringer":
+        realm_name = "dawnbringer"
+    elif arg == "deathwing":
+        realm_name = "deathwing"
+    elif arg == "demon soul":
+        realm_name = "demon_soul"
+    elif arg == "dentarg":
+        realm_name = "dentarg"
+    elif arg == "destromath":
+        realm_name = "destromath"
+    elif arg == "dethecus":
+        realm_name = "dethecus"
+    elif arg == "detheroc":
+        realm_name = "detheroc"
+    elif arg == "doomhammer":
+        realm_name = "doomhammer"
+    elif arg == "draenor":
+        realm_name = "draenor"
+    elif arg == "dragonblight":
+        realm_name = "dragonblight"
+    elif arg == "dragonmaw":
+        realm_name = "dragonmaw"
+    elif arg == "drak'tharon":
+        realm_name = "drak_tharon"
+    elif arg == "drak'thul":
+        realm_name = "drak_thul"
+    elif arg == "draka":
+        realm_name = "draka"
+    elif arg == "drenden":
+        realm_name = "drenden"
+    elif arg == "dunemaul":
+        realm_name = "dunemaul"
+    elif arg == "durotan":
+        realm_name = "durotan"
+    elif arg == "duskwood":
+        realm_name = "duskwood"
+    elif arg == "earthen ring":
+        realm_name = "earthen_ring"
+    elif arg == "echo isles":
+        realm_name = "echo_isles"
+    elif arg == "eitrigg":
+        realm_name = "eitrigg"
+    elif arg == "eldre'thalas":
+        realm_name = "eldre_thalas"
+    elif arg == "elune":
+        realm_name = "elune"
+    elif arg == "emerald dream":
+        realm_name = "emerald_dream"
+    elif arg == "eonar":
+        realm_name = "eonar"
+    elif arg == "eredar":
+        realm_name = "eredar"
+    elif arg == "executus":
+        realm_name = "executus"
+    elif arg == "exodar":
+        realm_name = "exodar"
+    elif arg == "farstriders":
+        realm_name = "farstriders"
+    elif arg == "feathermoon":
+        realm_name = "feathermoon"
+    elif arg == "fenris":
+        realm_name = "fenris"
+    elif arg == "firetree":
+        realm_name = "firetree"
+    elif arg == "fizzcrank":
+        realm_name = "fizzcrank"
+    elif arg == "frostmane":
+        realm_name = "frostmane"
+    elif arg == "frostwolf":
+        realm_name = "frostwolf"
+    elif arg == "galakrond":
+        realm_name = "galakrond"
+    elif arg == "garithos":
+        realm_name = "garithos"
+    elif arg == "garona":
+        realm_name = "garona"
+    elif arg == "garrosh":
+        realm_name = "garrosh"
+    elif arg == "ghostlands":
+        realm_name = "ghostlands"
+    elif arg == "gilneas":
+        realm_name = "gilneas"
+    elif arg == "gnomeregan":
+        realm_name = "gnomeregan"
+    elif arg == "gorefiend":
+        realm_name = "gorefiend",
+    elif arg == "gorgonnash":
+        realm_name = "gorgonnash"
+    elif arg == "greymane":
+        realm_name = "greymane"
+    elif arg == "grizzly hills":
+        realm_name = "grizzly_hills"
+    elif arg == "gul'dan":
+        realm_name = "gul_dan"
+    elif arg == "gurubashi":
+        realm_name = "gurubashi"
+    elif arg == "hakkar":
+        realm_name = "hakkar"
+    elif arg == "haomarush":
+        realm_name = "haomarush"
+    elif arg == "hellscream":
+        realm_name = "hellscream"
+    elif arg == "hydraxis":
+        realm_name = "hydraxis"
+    elif arg == "hyjal":
+        realm_name = "hyjal"
+    elif arg == "icecrown":
+        realm_name = "icecrown"
+    elif arg == "illidan":
+        realm_name = "illidan"
+    elif arg == "jaedenar":
+        realm_name = "jaedenar"
+    elif arg == "kael'thas":
+        realm_name = "kael_thas"
+    elif arg == "kalecgos":
+        realm_name = "kalecgos"
+    elif arg == "kargath":
+        realm_name = "kargath"
+    elif arg == "kel'thuzad":
+        realm_name = "kel_thuzad"
+    elif arg == "khadgar":
+        realm_name = "khadgar"
+    elif arg == "khaz modan":
+        realm_name = "khaz_modan"
+    elif arg == "kil'jaeden":
+        realm_name = "kil_jaeden"
+    elif arg == "kilrogg":
+        realm_name = "kilrogg"
+    elif arg == "korgath":
+        realm_name = "korgath"
+    elif arg == "korialstrasz":
+        realm_name = "korialstrasz"
+    elif arg == "kul tiras":
+        realm_name = "kul_tiras"
+    elif arg == "laughing skull":
+        realm_name = "laughing_skull"
+    elif arg == "lethon":
+        realm_name = "lethon"
+    elif arg == "lightbringer":
+        realm_name = "lightbringer"
+    elif arg == "lightninmaelstrom":
+        realm_name = "lightninmaelstrom"
+    elif arg == "lightning's blade":
+        realm_name = "lightnings_blade"
+    elif arg == "lightninghoof":
+        realm_name = "lightninghoof",
+    elif arg == "llane":
+        realm_name = "llane"
+    elif arg == "lothar":
+        realm_name = "lothar"
+    elif arg == "madoran":
+       realm_name = "madoran"
+    elif arg == "maelstrom":
+        realm_name = "maelstrom"
+    elif arg == "magtheridon":
+        realm_name = "magtheridon"
+    elif arg == "maiev":
+        realm_name = "maiev"
+    elif arg == "mal'ganis":
+        realm_name = "mal_ganis"
+    elif arg == "malfurion":
+        realm_name = "malfurion"
+    elif arg == "malorne":
+        realm_name = "malorne"
+    elif arg == "malygos":
+        realm_name = "malygos"
+    elif arg == "mannoroth":
+        realm_name = "mannoroth"
+    elif arg == "medivh":
+        realm_name = "medivh"
+    elif arg == "misha":
+        realm_name = "misha"
+    elif arg == "mok'nathal":
+        realm_name = "mok_nathal"
+    elif arg == "moon guard":
+        realm_name = "moon_guard"
+    elif arg == "moonrunner":
+        realm_name = "moonrunner"
+    elif arg == "mug'thol":
+        realm_name = "mug_thol"
+    elif arg == "muradin":
+        realm_name = "muradin"
+    elif arg == "nathrezim":
+        realm_name = "nathrezim"
+    elif arg == "nazgrel":
+        realm_name = "nazgrel"
+    elif arg == "nazjatar":
+        realm_name = "nazjatar"
+    elif arg == "ner'zhul":
+        realm_name = "ner_zhul"
+    elif arg == "nesingwary":
+        realm_name = "nesingwary"
+    elif arg == "nordrassil":
+        realm_name = "nordrassil"
+    elif arg == "norgannon":
+        realm_name = "norgannon"
+    elif arg == "onyxia":
+        realm_name = "onyxia"
+    elif arg == "perenolde":
+        realm_name = "perenolde"
+    elif arg == "proudmoore":
+        realm_name = "proudmoore"
+    elif arg == "quel'thalas":
+        realm_name = "quel_thalas"
+    elif arg == "quel'dorei":
+        realm_name = "quel_dorei"
+    elif arg == "ragnaros":
+        realm_name = "ragnaros"
+    elif arg == "ravencrest":
+        realm_name = "ravencrest"
+    elif arg == "ravenholdt":
+        realm_name = "ravenholdt"
+    elif arg == "rexxar":
+        realm_name = "rexxar"
+    elif arg == "rivendare":
+        realm_name = "rivendare"
+    elif arg == "runetotem":
+        realm_name = "runetotem"
+    elif arg == "sargeras":
+        realm_name = "sargeras"
+    elif arg == "saurfang":
+        realm_name = "saurfang"
+    elif arg == "scarlet crusade":
+        realm_name = "scarlet_crusade"
+    elif arg == "scolla":
+        realm_name = "scolla"
+    elif arg == "sen'jin":
+        realm_name = "sen_jin"
+    elif arg == "sentinels":
+        realm_name = "sentinels"
+    elif arg == "shadow council":
+        realm_name = "shadow_council"
+    elif arg == "shadowmoon":
+        realm_name = "shadowmoon"
+    elif arg == "shadowsong":
+        realm_name = "shadowsong"
+    elif arg == "shandris":
+        realm_name = "shandris"
+    elif arg == "shattered halls":
+        realm_name = "shattered_halls"
+    elif arg == "shattered hand":
+        realm_name = "shattered_hand"
+    elif arg == "shu'halo":
+        realm_name = "shu_halo"
+    elif arg == "silver hand":
+        realm_name = "silver_hand"
+    elif arg == "silvemoon":
+        realm_name = "silvemoon"
+    elif arg == "sister of elune":
+        realm_name = "sister_of_elune"
+    elif arg == "skullcrusher":
+        realm_name = "skullcrusher"
+    elif arg == "skywall":
+        realm_name = "skywall"
+    elif arg == "smolderthorn":
+        realm_name = "smolderthorn"
+    elif arg == "spinebreaker":
+        realm_name = "spinebreaker"
+    elif arg == "spirestone":
+        realm_name = "spirestone"
+    elif arg == "staghelm":
+        realm_name = "staghelm"
+    elif arg == "steamwheedle cartel":
+        realm_name = "steamwheedle_cartel"
+    elif arg == "stonemaul":
+        realm_name = "stonemaul"
+    elif arg == "stormrage":
+        realm_name = "stormrage"
+    elif arg == "stormreaver":
+        realm_name = "stormreaver"
+    elif arg == "stormscale":
+        realm_name = "stormscale"
+    elif arg == "suramar":
+        realm_name = "suramar"
+    elif arg == "tanaris":
+        realm_name = "tanaris"
+    elif arg == "terokkar":
+        realm_name = "terokkar"
+    elif arg == "thaurissan":
+        realm_name = "thaurissan"
+    elif arg == "the forgotten coast":
+        realm_name = "the_forgotten_coast"
+    elif arg == "the scryers":
+        realm_name = "the_scryers"
+    elif arg == "the underbog":
+        realm_name = "the_underbog"
+    elif arg == "the venture co":
+        realm_name = "the_venture_co"
+    elif arg == "thorium brotherhood":
+        realm_name = "thorium_brotherhood"
+    elif arg == "thrall":
+        realm_name = "thrall"
+    elif arg == "thunderhorn":
+        realm_name = "thunderhorn"
+    elif arg == "thunderlord":
+        realm_name = "thunderlord"
+    elif arg == "tichondrius":
+        realm_name = "tichondrius"
+    elif arg == "tol barad":
+        realm_name = "tol_barad"
+    elif arg == "tortheldrin":
+        realm_name = "tortheldrin"
+    elif arg == "trollbane":
+        realm_name = "trollbane"
+    elif arg == "turalyon":
+        realm_name = "turalyon"
+    elif arg == "twisting nether":
+        realm_name = "twisting_nether"
+    elif arg == "uldaman":
+        realm_name = "uldaman"
+    elif arg == "undermine":
+        realm_name = "undermine"
+    elif arg == "ursin":
+        realm_name = "ursin"
+    elif arg == "uther":
+        realm_name = "uther"
+    elif arg == "vashj":
+        realm_name = "vashj"
+    elif arg == "vek'nilash":
+        realm_name = "vek_nilash"
+    elif arg == "velen":
+        realm_name = "velen"
+    elif arg == "warsong":
+        realm_name = "warsong"
+    elif arg == "whisperwind":
+        realm_name = "whisperwind"
+    elif arg == "wildhammer":
+        realm_name = "wildhammer"
+    elif arg == "windrunner":
+        realm_name = "windrunner"
+    elif arg == "winterhoof":
+        realm_name = "winterhoof"
+    elif arg == "wyrmrest accord":
+        realm_name = "wyrmrest_accord"
+    elif arg == "ysera":
+        realm_name = "ysera"
+    elif arg == "ysondre":
+        realm_name = "ysondre"
+    elif arg == "zangarmarsh":
+        realm_name = "zangarmarsh"
+    elif arg == "zul'jin":
+        realm_name = "zul_jin"
+    elif arg == "zuluhead":
+        realm_name = "zuluhead"
+
+    return realm_name
+
